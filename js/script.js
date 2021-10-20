e800f455080271a22a1c19948f8388b498237329
Felix Milgo <felixmilgo21@gmail.com>
2021-10-20 14:29:51 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Challenge 1: Your age in days
+
+function ageInDays(){
+    var birthYear = prompt('Which year were you born?');
+    var noOfDays = (2021- birthYear) * 365;
+    var h1= document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + noOfDays + ' days old');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
+
+//Challenge 2: Cat Generator
